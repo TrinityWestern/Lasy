2eb25f4e3b6f05c0036cc47feeea06e515cb2940
badjer <brian.dejong@gmail.com>
2011-10-05 15:43:19 -0700

    Added another overload for Update

--- src/IWritableExtensions.cs
+++ src/IWritableExtensions.cs
@@ -17,10 +17,16 @@ namespace Lasy
             return writer.Insert(tablename, obj as Dictionary<string, object> ?? obj._AsDictionary(), trans).Single().Value.ConvertTo<int>();
         }
 
-        public static void Update(this IWriteable writer, string tablename, object obj, ITransaction trans = null)
+        /// <summary>
+        /// Update, and figure out the keys to use by analyzing the database for the primary key names. If the primary keys
+        /// are not supplied in values, an exception will be thrown
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="tablename"></param>
+        /// <param name="values"></param>
+        /// <param name="trans"></param>
+        public static void Update(this IWriteable writer, string tablename, Dictionary<string, object> values, ITransaction trans = null)
         {
-            var values = obj as Dictionary<string, object> ?? obj._AsDictionary();
-
             var keynames = writer.Analyzer.GetPrimaryKeys(tablename);
             // Make sure they've supplied all the keys
             if (!values.Keys.ToSet().IsSupersetOf(keynames))
@@ -29,6 +35,36 @@ namespace Lasy
             var keys = values.WhereKeys(k => keynames.Contains(k));
 
             writer.Update(tablename, values, keys, trans);
+        }
+
+        /// <summary>
+        /// Update, and figure out the keys to use by analyzing the database for the primary key names. If the primary keys
+        /// are not supplied in obj, an exception will be thrown
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="tablename"></param>
+        /// <param name="obj"></param>
+        /// <param name="trans"></param>
+        public static void Update(this IWriteable writer, string tablename, object obj, ITransaction trans = null)
+        {
+            var values = obj as Dictionary<string, object> ?? obj._AsDictionary();
+
+            Update(writer, tablename, values, trans);
+        }
+
+        /// <summary>
+        /// Calls Update after converting dataObj and keysObj to Dictionaries
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="tablename"></param>
+        /// <param name="dataObj">If Dict[string,object] it will be passed through, otherwise converted</param>
+        /// <param name="keysObj">If Dict[string,object] it will be passed through, otherwise converted</param>
+        /// <param name="trans"></param>
+        public static void Update(this IWriteable writer, string tablename, object dataObj, object keysObj, ITransaction trans = null)
+        {
+            Dictionary<string,object> data = dataObj as Dictionary<string, object> ?? dataObj._AsDictionary();
+            Dictionary<string,object> keys = keysObj as Dictionary<string, object> ?? keysObj._AsDictionary();
+            writer.Update(tablename, data, keys, trans);
         }
 
         public static void Delete(this IWriteable writer, string tablename, object obj, ITransaction trans = null)
