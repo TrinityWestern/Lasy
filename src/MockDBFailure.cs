586ebd752cd7c9c60cfaacf0c395235834cec0c7
badjer <brian.dejong@gmail.com>
2011-10-26 14:13:54 -0700

    Created UnreliableDb for mocking DB failures

---
+++ src/MockDBFailure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lasy
+{
+    public class MockDBFailure : Exception
+    {
+        public MockDBFailure()
+            : base()
+        { }
+
+        public MockDBFailure(string message)
+            : base(message)
+        { }
+    }
+}
---
+++ src/UnreliableDb.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lasy;
+
+namespace Lasy
+{
+    public class UnreliableDb : FakeDB
+    {
+        public UnreliableDb()
+            : base()
+        { }
+
+        public UnreliableDb(IDBAnalyzer analyzer)
+            : base(analyzer)
+        { }
+
+        public bool FailOnNextOp = false;
+
+        protected void _checkFail()
+        {
+            if (FailOnNextOp)
+            {
+                FailOnNextOp = false;
+                throw new MockDBFailure();
+            }
+        }
+
+        public override void Delete(string tableName, Dictionary<string, object> fieldValues, ITransaction transaction = null)
+        {
+            _checkFail();
+            base.Delete(tableName, fieldValues, transaction);
+        }
+
+        public override Dictionary<string, object> Insert(string tableName, Dictionary<string, object> row, ITransaction transaction = null)
+        {
+            _checkFail();
+            return base.Insert(tableName, row, transaction);
+        }
+
+        public override IEnumerable<Dictionary<string, object>> RawRead(string tableName, Dictionary<string, object> id, ITransaction transaction = null)
+        {
+            _checkFail();
+            return base.RawRead(tableName, id, transaction);
+        }
+
+        public override IEnumerable<Dictionary<string, object>> RawReadAll(string tableName, ITransaction transaction = null)
+        {
+            _checkFail();
+            return base.RawReadAll(tableName, transaction);
+        }
+
+        public override IEnumerable<Dictionary<string, object>> RawReadAllCustomFields(string tableName, IEnumerable<string> fields, ITransaction transaction = null)
+        {
+            _checkFail();
+            return base.RawReadAllCustomFields(tableName, fields, transaction);
+        }
+
+        public override IEnumerable<Dictionary<string, object>> RawReadCustomFields(string tableName, IEnumerable<string> fields, Dictionary<string, object> id, ITransaction transaction = null)
+        {
+            _checkFail();
+            return base.RawReadCustomFields(tableName, fields, id, transaction);
+        }
+
+        public override void Update(string tableName, Dictionary<string, object> dataFields, Dictionary<string, object> keyFields, ITransaction transaction = null)
+        {
+            _checkFail();
+            base.Update(tableName, dataFields, keyFields, transaction);
+        }
+    }
+}
