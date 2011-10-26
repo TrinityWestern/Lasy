bc32e97a3bcb4ff1ce5d572a14ba832db15644a8
badjer <brian.dejong@gmail.com>
2011-10-26 12:47:35 -0700

    Created LockBox - used for wrapping lockable rows in a disposable object so the rows get unlocked after you're done

---
+++ src/LockBox.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Nvelope;
+using Nvelope.Reflection;
+
+namespace Lasy
+{
+    /// <summary>
+    /// Provides a disposable wrapper for locking rows in a database. When the object is 
+    /// disposed, the rows are unlocked. Note: Assumes a LockId and LockDate field on 
+    /// the table it's used on
+    /// </summary>
+    /// <remarks>Best practice is to use this object in a using block, so you release
+    /// the locks as soon as your done your operations</remarks>
+    public class LockBox : IDisposable
+    {
+        public LockBox(IReadWrite db, string tablename)
+        {
+            Db = db;
+            Tablename = tablename;
+            LockId = Guid.NewGuid().ToString();
+        }
+
+        public string LockId { get; protected set; }
+        public IReadWrite Db { get; protected set; }
+        public string Tablename { get; protected set; }
+
+        /// <summary>
+        /// Read and lock some rows from the db. Only returns those rows that were successfully
+        /// locked.
+        /// </summary>
+        /// <param name="criteria">An object containing key-value pairs indicating the rows we want to return.
+        /// ie  {Processed = false}</param>
+        /// <param name="lockDate"></param>
+        /// <returns></returns>
+        public IEnumerable<Dictionary<string, object>> LockRead(object criteria, DateTime? lockDate = null)
+        {
+            lockDate = lockDate ?? DateTime.Now;
+
+            // Only lock things that are not currently locked,
+            // otherwise we might end up whacking someone else's locks,
+            // which would be a Bad Thing (tm)
+            var lockCriteria = criteria._AsDictionary().Assoc("LockId", null);
+
+            // Try to write the lock to the rows in the db
+            Db.Update(Tablename, new { LockId = LockId, LockDate = lockDate.Value }, lockCriteria);
+
+            // Get all the rows that we successfully locked
+            // Don't use the lock date, just the lockId, because SQL Server will 
+            // truncate datetimes, so the values won't match
+            var fromDb = Db.Read(Tablename, lockCriteria.Assoc("LockId", LockId));
+
+            return fromDb;
+        }
+
+        /// <summary>
+        /// Clears the lock
+        /// </summary>
+        public void Unlock()
+        {
+            // Clear the lockDate and LockId for any row that has this lockId
+            var updateData = new { LockId = DBNull.Value, LockDate = DBNull.Value };
+            var keys = new { LockId = LockId };
+            Db.Update(Tablename, updateData, keys);
+        }
+
+
+        public void Dispose()
+        {
+            Unlock();
+        }
+    }
+}
---
+++ test/LockBoxTests.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Lasy;
+using Nvelope;
+
+namespace LasyTests
+{
+    [TestFixture]
+    public class LockBoxTests
+    {
+        protected object _criteria
+        {
+            get
+            {
+                return new { ShouldProcess = true };
+            }
+        }
+
+        protected FakeDB _setup()
+        {
+            var db = new FakeDB();
+            db.Insert("Tbl", new { ShouldProcess = true, LockId = DBNull.Value, LockDate = DBNull.Value });
+            return db;
+        }
+
+        [Test]
+        public void GeneratesUniqueLocks()
+        {
+            var db = _setup();
+            var locks = 1.To(10).Select(i => new LockBox(db, "Tbl").LockId);
+
+            Assert.False(locks.Any(l => l.IsNullOrEmpty()), "None of the locks should have been empty");
+            Assert.False(locks.AreAllEqual(), "Locks should all have been different, but were: " + locks.Print());
+        }
+
+        [Test]
+        public void LocksRows()
+        {
+            var db = _setup();
+
+
+            var lb = new LockBox(db, "Tbl");
+            var rows = lb.LockRead(_criteria);
+            var rowLocks = rows.Select(r => r["LockId"].ToString());
+            foreach (var rowLock in rowLocks)
+                Assert.AreEqual(lb.LockId, rowLock, "Expected each row to have the lockId set");
+
+            var fromDb = db.Read("Tbl", _criteria);
+            var dbLocks = fromDb.Select(r => r["LockId"].ToString());
+            foreach (var dbLock in dbLocks)
+                Assert.AreEqual(lb.LockId, dbLock, "Expected each database row to have the lockId set");
+        }
+
+        [Test]
+        public void SetsLockDate()
+        {
+            var db = _setup();
+
+            var lb = new LockBox(db, "Tbl");
+            var rows = lb.LockRead(_criteria);
+            Assert.NotNull(rows.Single()["LockDate"]);
+
+            var fromDb = db.Read("Tbl", _criteria);
+            Assert.NotNull(fromDb.Single()["LockDate"]);
+        }
+
+
+        [Test]
+        public void WontLockLockedRows()
+        {
+            var db = _setup();
+
+            var lb = new LockBox(db, "Tbl");
+            var rows = lb.LockRead(_criteria);
+            Assert.AreNotEqual(0, rows.Count(), "We should have found rows on the first lock");
+
+            var lb2 = new LockBox(db, "Tbl");
+            var rows2 = lb2.LockRead(_criteria);
+            Assert.AreEqual(0, rows2.Count(), "We shouldn't have found any rows on the second lock");
+        }
+
+        [Test]
+        public void UsingBlockClearsLock()
+        {
+            var db = _setup();
+
+            using (var lb = new LockBox(db, "Tbl"))
+            {
+                var rows = lb.LockRead(_criteria);
+                Assert.AreNotEqual(0, rows.Count(), "We should have found rows");
+            }
+
+            var lb2 = new LockBox(db, "Tbl");
+            var rows2 = lb2.LockRead(_criteria);
+            Assert.AreNotEqual(0, rows2.Count(), "The old lock went out of scope, so we should find rows here - they should be unlocked");
+        }
+    }
+}
