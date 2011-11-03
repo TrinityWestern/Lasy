852bef7ba059119911f280c108b89b260b64d16c
badjer <brian.dejong@gmail.com>
2011-11-03 09:11:20 -0700

    Tests for SQL meta-operations

---
+++ test/SQLDBAnalyzerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace LasyTests
+{
+    [TestFixture]
+    public class SQLDBAnalyzerTests
+    {
+        [Test]
+        public void TableExists()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void SchemaExists()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ test/TableCreationTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace LasyTests
+{
+    [TestFixture]
+    public class TableCreationTests
+    {
+        [Test]
+        public void CreatesTable()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void CreatesSchema()
+        {
+            Assert.Fail();
+        }
+
+        [Test(Description="If CreateTables mode is False, throw an exception if you try to write to a non-existant table")]
+        public void InsertThrowsExceptionIfNoTable()
+        {
+            Assert.Fail();
+        }
+
+        [Test(Description="If EnforceTables mode is True, throw an exception if you try to read from a non-existant table")]
+        public void ReadThrowsExceptionIfNoTable()
+        {
+            Assert.Fail();
+        }
+
+
+    }
+}
