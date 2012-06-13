468a33aff76a0a487456be3dd58b548a255c44e8
badjer <brian.dejong@gmail.com>
2012-06-13 16:27:17 -0700

    Basic tests for mySQL CRUD operations

--- test/Sql/MySqlTests.cs
+++ test/Sql/MySqlTests.cs
@@ -4,42 +4,55 @@ using System.Linq;
 using System.Text;
 using NUnit.Framework;
 using Lasy;
+using Nvelope;
+using Nvelope.Reflection;
 
 namespace LasyTests.Sql
 {
     [TestFixture]
     public class MySqlTests
     {
+        protected string _connStr = "Server=localhost;Database=lasytest;Uid=lasy;Pwd=abc123;";
+
         [Test]
-        public void ConnectstoMySQL()
+        public void Read()
         {
-            var db = ConnectTo.MySql("Server=localhost;Database=lasytest;Uid=lasy;Pwd=abc123;");
+            var db = ConnectTo.MySql(_connStr);
             var res = db.ReadAll("House");
             Assert.True(res.Any());
         }
 
         [Test]
-        public void Read()
-        {
-            Assert.Fail();
-        }
-
-        [Test]
         public void Insert()
         {
-            Assert.Fail();
+            var db = ConnectTo.MySql(_connStr);
+            var obj = new { Number = 1, Street = "Main"};
+            var keys = db.Insert("House", obj);
+            var fromDb = db.Read("House", keys).Single();
+            Assert.True(obj._AsDictionary().IsSameAs(fromDb));
         }
 
         [Test]
         public void Update()
         {
-            Assert.Fail();
+            var db = ConnectTo.MySql(_connStr);
+            var obj = new { Number = 1, Street = "Main" };
+            var keys = db.Insert("House", obj);
+            var newObj = obj._AsDictionary().Assoc("Number", 2);
+            db.Update("House", newObj, keys);
+            var fromDb = db.Read("House", keys).Single();
+            Assert.True(newObj.IsSameAs(fromDb));
         }
 
         [Test]
         public void Delete()
         {
-            Assert.Fail();
+            var db = ConnectTo.MySql(_connStr);
+            var obj = new { Number = 1, Street = "Main" };
+            var keys = db.Insert("House", obj);
+            Assert.True(db.Read("House", keys).Any());
+            db.Delete("House", keys);
+            Assert.False(db.Read("House", keys).Any());
         }
     }
 }
