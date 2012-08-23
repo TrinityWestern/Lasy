eaec6ce8d0a7cbdd422c05bd6a1f5ca6eacf07d4
badjer <brian.dejong@gmail.com>
2012-08-23 10:04:30 -0700

    Deal with nested transactions issue

--- src/MySqlDB.cs
+++ src/MySqlDB.cs
@@ -34,5 +34,56 @@ namespace Lasy
         {
             return "; SELECT LAST_INSERT_ID();";
         }
+
+        #region Temporary Transaction fix
+#warning Remove this once nested transaction issue fixed
+
+        public override ITransaction BeginTransaction()
+        {
+            return new NonTransaction(this);
+        }
+
+        public class NonTransaction : ITransaction
+        {
+            public NonTransaction(MySqlDB db)
+            {
+                _db = db;
+            }
+
+            public MySqlDB _db;
+
+            public void Commit() { }
+
+            public void Rollback() { }
+
+            public IEnumerable<Dictionary<string, object>> RawRead(string tableName, Dictionary<string, object> keyFields, IEnumerable<string> fields = null)
+            {
+                return _db.RawRead(tableName, keyFields, fields);
+            }
+
+            public IDBAnalyzer Analyzer
+            {
+                get { return _db.Analyzer; }
+            }
+
+            public Dictionary<string, object> Insert(string tableName, Dictionary<string, object> row)
+            {
+                return _db.Insert(tableName, row);
+            }
+
+            public void Delete(string tableName, Dictionary<string, object> keyFields)
+            {
+                _db.Delete(tableName, keyFields);
+            }
+
+            public void Update(string tableName, Dictionary<string, object> dataFields, Dictionary<string, object> keyFields)
+            {
+                _db.Update(tableName, dataFields, keyFields);
+            }
+
+            public void Dispose() { }
+        }
+
+        #endregion
     }
 }
